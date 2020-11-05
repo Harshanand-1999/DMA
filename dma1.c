bc3c65dd18d0c0cd04f51cfe22679d0527fcf973
Harsh <harshanand1198@gmail.com>
2020-11-05 08:38:46 +0530

    this is the first commit

---
+++ dma1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+int i, n, *ptr;
+printf("enter the number of elements\n");
+scanf("%d",&n);
+ptr=(int*)malloc(n*sizeof(int));
+printf("enter the elements");
+for(i=0;i<n;i++)
+scanf("%d",ptr+i);
+for(i=0;i<n;i++)
+printf("%d",*(ptr+i));
+
+}
